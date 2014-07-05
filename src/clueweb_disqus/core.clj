bb383c17c96121343b4190d5198a7ac51f29288a
Shriphani Palakodety <shriphanip@gmail.com>
2014-07-05 12:24:30 -0400

    gets first page of post listing

--- src/clueweb_disqus/core.clj
+++ src/clueweb_disqus/core.clj
@@ -1,6 +1,28 @@
-(ns clueweb-disqus.core)
+(ns clueweb-disqus.core
+  (:require [cheshire.core :refer :all]
+            [clj-http.client :as client]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(def thread-request-url "https://disqus.com/api/3.0/threads/list.json")
+
+(def clueweb-start-date-epoch "1328832000")
+
+(defn load-credentials
+  []
+  (-> "credentials.clj"
+      slurp
+      read-string))
+
+(defn create-url
+  [since-epoch]
+  (let [credentials (load-credentials)]
+   (str thread-request-url
+        "?api_key="
+        (-> credentials (get since-epoch) :api-key)
+        "&since="
+        since-epoch)))
+
+(defn threads-since
+  [start-epoch]
+  (let [request-url (create-url start-epoch)
+        first-page  (-> request-url client/get :body parse-string)]
+    first-page))
