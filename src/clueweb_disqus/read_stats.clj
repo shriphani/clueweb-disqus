98f2097e7ac0b33e876f8eb7b536b70c1d31161e
Shriphani Palakodety <shriphanip@gmail.com>
2014-07-07 17:46:30 -0400

    added html dumping

--- src/clueweb_disqus/read_stats.clj
+++ src/clueweb_disqus/read_stats.clj
@@ -2,7 +2,8 @@
   "Read stats + make a graph"
   (:gen-class :main true)
   (:require [clj-time.core :as t]
-            [clj-time.coerce :as c])
+            [clj-time.coerce :as c]
+            [clojure.java.io :as io])
   (:use [incanter core stats charts io]))
 
 (defn compute-downloaded-records
@@ -17,6 +18,32 @@
      0
      stats-files)))
 
+(defn generate-samples
+  []
+  (let [data-files (filter
+                     (fn [f]
+                       (re-find #".clj$" (.getAbsolutePath f)))
+                     (file-seq (java.io.File. ".")))]
+    (str
+     "<html>"
+     "<body>"
+     "<pre>"
+     (clojure.string/join
+      "\n"
+      (map
+       (fn [a-data-file]
+         (let [rdr (-> a-data-file io/reader java.io.PushbackReader.)
+               stream (take-while
+                       identity
+                       (repeatedly
+                        (fn [] (try (read rdr)
+                                   (catch Exception e nil)))))]
+           (last stream)))
+       data-files))
+     "</pre>"
+     "</body>"
+     "</html>")))
+
 (defn -main
   [& args]
   (spit "disqus_stats.csv"
@@ -26,8 +53,9 @@
              (compute-downloaded-records)
              "\n")
         :append true)
-  (let [data (read-dataset "disqus_stats.csv")
-        dates (sel data :cols 0)
-        cnt (sel data :cols 1)]
-    (save (time-series-plot dates cnt :y-label "Threads List Downloaded")
-          "/bos/www/htdocs/spalakod/disqus/disqus_thread_list.png")))
+  (do (let [data (read-dataset "disqus_stats.csv")
+            dates (sel data :cols 0)
+            cnt (sel data :cols 1)]
+        (save (time-series-plot dates cnt :y-label "Threads List Downloaded")
+              "/bos/www/htdocs/spalakod/disqus/disqus_thread_list.png"))
+      (spit "disqus_samples.html" (generate-samples))))
