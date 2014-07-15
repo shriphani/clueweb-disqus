72a01da9e11ea105d35853388bb5b976b3e72bf9
Shriphani Palakodety <shriphanip@gmail.com>
2014-07-15 18:12:11 -0400

    recover quicker and better

--- src/clueweb_disqus/recover.clj
+++ src/clueweb_disqus/recover.clj
@@ -1,18 +1,72 @@
 (ns clueweb-disqus.recover
   "Module to restart crawls"
-  (:require [clojure.string :as string]
-            [clueweb-disqus.core :as core])
+  (:require [clj-time.coerce :as c]
+            [clj-time.format :as f]
+            [clj-time.core :as t]
+            [clojure.java.io :as io]
+            [clojure.string :as string]
+            [clueweb-disqus.core :as core]
+            [clueweb-disqus.posts :as posts])
   (:use [clojure.java.shell :only [sh]])
   (:import [java.lang ProcessBuilder]))
 
-(defn to-recover
+(defn list-disqus-files
   []
-  (string/split-lines
-   (slurp core/recovery-file)))
+  (map
+   #(.getAbsolutePath %)
+   (filter
+    (fn [f]
+      (and
+       (not
+        (re-find #"bkup"
+                 (.getAbsolutePath f)))
+       (re-find #".*-disqus-threads.*.clj"
+                (.getAbsolutePath f))))
+    (file-seq
+     (io/as-file core/disqus-jobs-dir)))))
 
-(defn restart-to-recover-jobs
+(defn potential-restart-point
+  [a-disqus-file]
+  (let [formatter   (f/formatters :date-hour-minute-second)]
+    (quot (c/to-long
+           (f/parse formatter
+                    (last
+                     (map
+                      #(get % "createdAt")
+                      (posts/threads-list-seq a-disqus-file)))))
+          1000)))
+
+(defn potential-restart-points
   []
-  (do (let [to-recover-list (to-recover)]
-        (doseq [disqus-proc to-recover-list]
-          (sh "lein" (str "run -r " disqus-proc))))
-      (spit core/recovery-file "")))
+  (pmap potential-restart-point
+        (list-disqus-files)))
+
+(defn dump-restart-points
+  []
+  (let [credentials  (core/load-credentials)
+        starts       (keys credentials)
+        starts-stops (map
+                      (fn [x]
+                        [(Long/parseLong x)
+                         (Long/parseLong
+                          (:stop
+                           (credentials x)))])
+                      starts)
+
+        restart-points (potential-restart-points)
+
+        chosen-restart-epochs (pmap
+                               (fn [[start stop]]
+                                 (apply
+                                  max
+                                  (filter
+                                   #(and
+                                     (<= start %)
+                                     (> stop %))
+                                   restart-points)))
+                               starts-stops)]
+    
+    (with-open [wrtr (io/writer (str core/disqus-jobs-dir "recover.clj"))]
+     (doseq [epoch chosen-restart-epochs]
+       (binding [*out* wrtr]
+        (println epoch))))))
