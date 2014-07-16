6e00fcf2fac168aba15179294c352be5553ee820
Shriphani Palakodety <shriphanip@gmail.com>
2014-07-16 14:17:01 -0400

    runs command to dump to recover.clj directly

--- recover.sh
+++ recover.sh
@@ -2,6 +2,8 @@
 # Script to read the recovery file and
 # restart the crawl from there
 
+lein trampoline run -b
+
 recovery_file="/bos/tmp19/spalakod/clueweb12pp/disqus/recover.clj"
 
 for line in $(cat $recovery_file);
